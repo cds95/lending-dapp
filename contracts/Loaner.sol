03dcc0a9927f4ea3c3e2a4b4cc9e61f606a46c89
cds95 <cds95@uw.edu>
2020-12-17 21:03:13 -0800

    Implement giving and withdrawing loans in contract

--- contracts/Loaner.sol
+++ contracts/Loaner.sol
@@ -2,16 +2,23 @@ pragma solidity ^0.5.0;
 
 contract Loaner {
     struct Loan {
-        uint256 id;
         uint256 amount;
         address borrower;
         address lender;
         uint256 interest;
+        bool hasBeenSettled;
     }
 
+    // loanId is the index of the loan
     Loan[] private loans;
     uint256 private numLoans;
     uint256 private idCounter;
+    mapping(address => uint256) balances;
+    mapping(address => uint256) pendingLoanWithdrawals;
+
+    function inputFunds() public payable {
+        pendingLoanWithdrawals[msg.sender] += msg.value;
+    }
 
     function getLoans() public view returns (uint256[] memory) {
         uint256[] memory loanAmounts = new uint256[](numLoans);
@@ -28,11 +35,34 @@ contract Loaner {
 
     function askForLoan(uint256 loanAmountInWei) public {
         Loan memory newLoan;
-        newLoan.id = idCounter;
         newLoan.borrower = msg.sender;
         newLoan.amount = loanAmountInWei;
-        loans[numLoans] = newLoan;
+        loans[idCounter] = newLoan;
         numLoans++;
         idCounter++;
+
+        // TODO:  Emit event saying new loan has appeared
+    }
+
+    function giveLoan(uint256 loanId) public {
+        Loan storage loan = loans[loanId];
+        if (balances[msg.sender] >= loan.amount) {
+            pendingLoanWithdrawals[loan.borrower] += loan.amount;
+            balances[msg.sender] -= loan.amount;
+            loan.lender = msg.sender;
+
+            // TODO: loan.interest = find out how to send data from UI to do this.
+            // TODO:  Emit event saying loan has been provided and needs to be withdrawn.
+            //
+        }
+    }
+
+    function withdrawLoan(uint256 loanId) public {
+        Loan memory loan = loans[loanId];
+        if (msg.sender == loan.borrower) {
+            uint256 amount = pendingLoanWithdrawals[msg.sender];
+            pendingLoanWithdrawals[msg.sender] = 0;
+            msg.sender.transfer(amount);
+        }
     }
 }
