9e106c957ab39dddbd51964fda1b8a6b7b9755be
cds95 <cds95@uw.edu>
2020-12-29 15:07:43 -0800

    add ropsten deployment config

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,10 @@
 const path = require("path");
+const HDWalletProvider = require("truffle-hdwallet-provider");
+require('dotenv').config()
 
+/**
+ * DONT FORGET TO SETUP .env FILE!!!
+ */
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -7,6 +12,13 @@ module.exports = {
   networks: {
     develop: {
       port: 7545
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(process.env.ROPSTEN_MNEMONIC, "https://ropsten.infura.io/v3/" + process.env.INFURA_APP_ID)
+      },
+      network_id: 3,
+      gas: 4000000      //make sure this gas allocation isn't over 4M, which is the max
     }
   }
 };
